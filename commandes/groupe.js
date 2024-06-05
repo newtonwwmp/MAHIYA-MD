c3fab7906050e4fdebecf16c9f2eee595dc380ec
newtonwwmp <161313291+newtonwwmp@users.noreply.github.com>
2024-06-05 20:15:48 +0530

    Update groupe.js

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *FEENIX-MD* 🌟
+        🌟 *MAHIYA-MD* 🌟
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Author : *${nomAuteurMessage}* 👋 
