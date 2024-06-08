a3d80299c08b2678b53ac21939f18496f7ec5e4d
newtonwwmp <161313291+newtonwwmp@users.noreply.github.com>
2024-06-08 11:54:31 +0530

    MAHIYA-MD

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'FEENIX-MD',
+        pack: 'MAHIYA-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
