cad3ddc9c8e6d5b8b8e33584cb3be508b91878bb
newtonwwmp <161313291+newtonwwmp@users.noreply.github.com>
2024-06-10 19:35:06 +0530

    MAHIYA-MD

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😾", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*FEENIX-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FEENIX-MD.*";
+    let z = '*FEENIX-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *MAHIYA-MD.*";
     let d = ' https://email10min.com';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/7a8bf9f622dd6ccb403df.mp4';
