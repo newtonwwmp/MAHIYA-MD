536dc6bcac847822e0c27d005611c6549506c355
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-28 19:12:13 +0530

    Update youtube.js

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -8,7 +8,7 @@ const yts1 = require("youtube-yts");
 //var fs =require("fs-extra")
 
 zokou({
-  nomCom: "song",
+  nomCom: "play",
   categorie: "Search",
   reaction: "💿"
 }, async (origineMessage, zk, commandeOptions) => {
