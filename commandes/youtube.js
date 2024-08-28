df916188fcd359f1e769c46548d204c4775ade75
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-28 18:39:28 +0530

    Update youtube.js

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -127,7 +127,7 @@ _*MAHIYA-MD On downloading...*_\n\n`
 
       fileStream.on('finish', () => {
         // Envoi du fichier vidéo en utilisant l'URL du fichier local
-        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*MAHIYA-MD*", gifPlayback: false }, { quoted: ms });
+        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*FEENIX-MD*", gifPlayback: false }, { quoted: ms });
       });
 
       fileStream.on('error', (error) => {
